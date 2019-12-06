23f26bb13a84f5a7b7c0f428d7a0a060b5d7d1a3
jakubg98 <gocek112@onet.pl>
2019-12-06 20:38:48 +0100

    added animation of falling object from the top

--- skupSie/MainWindow.xaml.cs
+++ skupSie/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,14 @@ namespace SkupSieGra
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            TranslateTransform trans = new TranslateTransform();
+            elipsa.RenderTransform = trans;
+            DoubleAnimation anim = new DoubleAnimation(29, 300, TimeSpan.FromSeconds(3));
+            trans.BeginAnimation(TranslateTransform.YProperty, anim);
         }
     }
 }
