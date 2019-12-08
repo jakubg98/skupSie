4a30e9792c850efc6f7b11d7ebc90ffaa55e3935
jakubg98 <gocek112@onet.pl>
2019-12-08 10:46:28 +0100

    added some timer and random generator, trying to repeat falling of obstacles

--- skupSie/MainWindow.xaml.cs
+++ skupSie/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Timers;
 
 namespace SkupSieGra
 {
@@ -24,17 +25,50 @@ namespace SkupSieGra
         public MainWindow()
         {
             InitializeComponent();
+
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+
             this.KeyDown += new KeyEventHandler(OnButtonKeyDown);
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            lblSeconds.Content = DateTime.Now.Second;
+            Random rnd = new Random();
+            int x = rnd.Next(0, 2);
+            rndGen.Content = x;
+            if (x ==0)
+            {
+                obstacle2.Opacity = 0;
+                obstacle1.Opacity = 1;
+            }
+            if (x == 1)
+            {
+                    obstacle2.Opacity = 1;
+                    obstacle1.Opacity = 0;
+            }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             TranslateTransform trans = new TranslateTransform();
-            elipsa.RenderTransform = trans;
+            obstacle1.RenderTransform = trans;
+            obstacle2.RenderTransform = trans;
             DoubleAnimation anim = new DoubleAnimation(29, 300, TimeSpan.FromSeconds(3));
             trans.BeginAnimation(TranslateTransform.YProperty, anim);
 
-            //if(elipsa.Top>=)
+            //add fucntion that loops falling of obstacles
+
+            if (Canvas.GetTop(obstacle1) == 290) 
+            {
+                obstacle1.RenderTransform = trans;
+                obstacle2.RenderTransform = trans;
+                DoubleAnimation anim2 = new DoubleAnimation(29, 300, TimeSpan.FromSeconds(3));
+                trans.BeginAnimation(TranslateTransform.YProperty, anim2);
+            }
         }
 
         private void OnButtonKeyDown(object sender, KeyEventArgs e)
